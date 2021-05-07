dfc0576961a5b2e811eec96398203483b4ac35c8
Brian Bayarri <38357877+briezeba@users.noreply.github.com>
2021-05-07 15:38:07 -0300

    Update Constants.cs

--- LabelsSystem/Utils/Constants.cs
+++ LabelsSystem/Utils/Constants.cs
@@ -6,8 +6,8 @@ namespace LabelsSystem.Utils
 {
     public class Constants
     {
-        public static string TEMPLATE_PATH = "C:\\Brian\\workspace\\.NET\\ProyectoUSA\\Main\\LabelsSystem\\LabelsSystem\\Template\\Template.docx";
-        public static string TEMP_PATH = "C:\\Brian\\workspace\\.NET\\ProyectoUSA\\Main\\LabelsSystem\\LabelsSystem\\temp\\Result.docx";
-        public static string OUTPUT_PATH = "C:\\Brian\\workspace\\.NET\\ProyectoUSA\\Main\\LabelsSystem\\LabelsSystem\\Output\\Result.pdf";
+        public static string TEMPLATE_PATH = "...\\LabelsSystem\\Template\\Template.docx";
+        public static string TEMP_PATH = "...\\LabelsSystem\\temp\\Result.docx";
+        public static string OUTPUT_PATH = "...\\LabelsSystem\\Output\\Result.pdf";
     }
 }
